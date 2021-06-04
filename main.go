70e21558b2944fb4c76ca656a3a17ff1735a73e2
coolmsf <coolder.hsl@outlook.com>
2021-06-04 11:14:49 +0800

    add session support

--- main.go
+++ main.go
@@ -3,46 +3,51 @@ package main
 import (
 	"./controller/get"
 	"./controller/post"
-	_ "./middleware"
 	"./pkg"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 )
 
+
 func main() {
+	r := engine()
+	r.Use(gin.Logger())
+	if err := engine().RunTLS(":3333", "server.crt", "server.key"); err != nil {
+		log.Fatal("Unable to start:", err)
+	}
+}
 
-	r := gin.Default()
+func engine() *gin.Engine {
+	r := gin.New()
 
-	/*
-	 * HTML render
-	 */
+	r.Use(sessions.Sessions("mysession", sessions.NewCookieStore([]byte("secret"))))
 	r.LoadHTMLGlob("view/*")
+
 	r.GET("/", get.RenderIndex)
 	r.GET("/view/login.html", get.RenderLogin)
 	r.GET("/view/create.html", get.RenderCreate)
 	r.GET("/view/upload.html", get.RenderUpload)
+	r.GET("/view/gif.html", get.RenderGif)
+	r.GET("/view/webshell.html", get.RenderWebshell)
+	r.GET("/view/playground.html", get.RenderPlayground)
+	r.GET("/logout", pkg.Logout)
 
+	r.POST("/login", post.Login)
+	r.POST("/playground", post.Playground)
+	r.POST("/gif", post.Gif)
 	r.POST("/create", post.Create)
 
-	sess := r.Group("/", pkg.EnableCookieSession())
+	private := r.Group("/private")
+	// private.Use(AuthRequired)
+	private.Use(pkg.AuthSessionMiddle)
 	{
-		sess.POST("/login", post.Login)
-
-		/*
-		 * Got problem here, session not working...
-		 */
-		// authorized := sess.Group("/auth", pkg.AuthSessionMiddle())
-		authorized := sess.Group("/auth")
-		{
-			authorized.POST("/upload", post.Upload)
-			authorized.StaticFS("/download", http.Dir("download"))
-		}
+		private.GET("/me", pkg.Me)
+		private.GET("/status", pkg.Status)
+		private.POST("/upload", post.Upload)
+		private.POST("/webshell", post.WebShell)
+		private.StaticFS("/download", http.Dir("download"))
 	}
-
-	// err := r.Run(":3333")
-	err := r.RunTLS(":3333", "server.crt", "server.key")
-	if err != nil {
-		log.Fatal("r.Run")
-	}
+	return r
 }
--- pkg/session.go
+++ pkg/session.go
@@ -1,66 +1,86 @@
 package pkg
 
 import (
-    "github.com/gin-contrib/sessions"
-    "github.com/gin-contrib/sessions/cookie"
-    "github.com/gin-gonic/gin"
-    "net/http"
+"github.com/gin-gonic/contrib/sessions"
+"github.com/gin-gonic/gin"
+"log"
+"net/http"
+)
+
+const (
+	userkey = "user"
 )
 
 // gin session key
-const KEY = "AEN233"
+// const userkey = "user"
 
 func EnableCookieSession() gin.HandlerFunc {
-    store := cookie.NewStore([]byte(KEY))
-    return sessions.Sessions("mysession", store)
+	// store := sessions.NewCookieStore([]byte("secret"))
+	// store, _ := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
+	store, err := sessions.NewRedisStore(10, "tcp", "localhost:6379", "", []byte("secret"))
+	if err != nil {
+		log.Fatalf("sessions.NewRedisStore: %v", err)
+	}
+	return sessions.Sessions("mysession", store)
 }
 
-func AuthSessionMiddle() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        session := sessions.Default(c)
-        sessionValue := session.Get("userId")
-        if sessionValue == nil {
-            c.JSON(http.StatusUnauthorized, gin.H{
-                "error": "Unauthorized",
-            })
-            c.Abort()
-            return
-        }
-        c.Set("userId", sessionValue.(uint))
-
-        c.Next()
-        return
-    }
+func AuthSessionMiddle(c *gin.Context) {
+	session := sessions.Default(c)
+	sessionValue := session.Get("user")
+	if sessionValue == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Unauthorized",
+		})
+		c.Abort()
+		return
+	}
+	c.Next()
+	return
 }
 
-func SaveAuthSession(c *gin.Context, id uint) {
-    session := sessions.Default(c)
-    session.Set("userId", id)
-    session.Save()
+// func AuthRequired(c *gin.Context) {
+//     session := sessions.Default(c)
+//     user := session.Get("user")
+//     if user == nil {
+//         // Abort the request with the appropriate error code
+//         c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+//         return
+//     }
+//     // Continue down the chain to handler etc
+//     c.Next()
+// }
+
+func SaveAuthSession(c *gin.Context, username string) {
+	session := sessions.Default(c)
+	session.Set("user", username)
+	err := session.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to save session"})
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "save session ok"})
 }
 
-// 退出时清除session
 func ClearAuthSession(c *gin.Context) {
-    session := sessions.Default(c)
-    session.Clear()
-    session.Save()
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
 }
 
 func HasSession(c *gin.Context) bool {
-    session := sessions.Default(c)
-    if sessionValue := session.Get("userId"); sessionValue == nil {
-        return false
-    }
-    return true
+	session := sessions.Default(c)
+	if sessionValue := session.Get("userId"); sessionValue == nil {
+		return false
+	}
+	return true
 }
 
 func GetSessionUserId(c *gin.Context) uint {
-    session := sessions.Default(c)
-    sessionValue := session.Get("userId")
-    if sessionValue == nil {
-        return 0
-    }
-    return sessionValue.(uint)
+	session := sessions.Default(c)
+	sessionValue := session.Get("userId")
+	if sessionValue == nil {
+		return 0
+	}
+	return sessionValue.(uint)
 }
 
 // func GetUserSession(c *gin.Context) map[string]interface{} {
@@ -76,3 +96,44 @@ func GetSessionUserId(c *gin.Context) uint {
 //     data["userName"] = userName
 //     return data
 // }
+
+
+// AuthRequired is a simple middleware to check the session
+func AuthRequired(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get(userkey)
+	if user == nil {
+		// Abort the request with the appropriate error code
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+	// Continue down the chain to handler etc
+	c.Next()
+}
+
+
+
+func Logout(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get(userkey)
+	if user == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+		return
+	}
+	session.Delete(userkey)
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+}
+
+func Me(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get(userkey)
+	c.JSON(http.StatusOK, gin.H{"user": user})
+}
+
+func Status(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"status": "You are logged in"})
+}
